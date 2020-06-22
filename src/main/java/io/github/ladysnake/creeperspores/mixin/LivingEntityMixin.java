188a56d223451f65123476ce94f9ca23ec5a196b
Pyrofab <redstoneinfire@gmail.com>
2020-06-22 23:18:38 +0200

    Update to 1.16-rc1

--- src/main/java/io/github/ladysnake/creeperspores/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/ladysnake/creeperspores/mixin/LivingEntityMixin.java
@@ -44,7 +44,7 @@ public abstract class LivingEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;method_29504()Z", ordinal = 1))
+    @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isDead()Z", ordinal = 1))
     private void spawnCreeperling(DamageSource cause, float amount, CallbackInfoReturnable<Boolean> cir) {
         for (CreeperEntry creeperEntry : CreeperEntry.all()) {
             StatusEffectInstance spores = this.getStatusEffect(creeperEntry.sporeEffect);
@@ -62,5 +62,4 @@ public abstract class LivingEntityMixin extends Entity {
             }
         }
     }
-
 }
